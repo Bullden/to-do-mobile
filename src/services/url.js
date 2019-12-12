22d79c4cd9f778c187a9a47b83c23c87bc074d84
Bullden <deniiserbull@gmail.com>
2019-12-12 14:16:17 +0200

    make it better

--- src/services/url.js
+++ src/services/url.js
@@ -1,9 +1,9 @@
-// export const urlApi = {
-//     api: 'https://todobackk.herokuapp.com'
-// }
 export const urlApi = {
-    api: 'http://localhost:3001'
+    api: 'https://todobackk.herokuapp.com'
 }
+// export const urlApi = {
+//     api: 'http://localhost:5000'
+// }
 export const urlSignIn = {
     api: 'https://accounts.google.com/o/oauth2/v2/auth?'
 }
