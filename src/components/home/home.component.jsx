d44765c67cd27443f469db963edd88def5009dbe
Bullden <deniiserbull@gmail.com>
2019-12-12 14:41:36 +0200

    make it better

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -20,7 +20,7 @@ export class HomeComponent extends React.Component {
     }
     render(){
          const signIn = this.props.signIn
-        return(
+        return( signIn ?
             <div>
                 <p className="p-name">Today</p>
                 <OpenListComponent/>
@@ -28,7 +28,7 @@ export class HomeComponent extends React.Component {
                 <SimpleList />
                 <AddButton />
                 <CardsComponent />
-            </div> 
+            </div> : <Redirect to='/sign-in' />
         )
     }
 }
